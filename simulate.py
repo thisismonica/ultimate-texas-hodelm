16cf181ef7399f9f989b34a396e066e78b76db4d
monica wang <qirongismonica@gmail.com>
2016-02-17 21:35:19 -0800

    add comment line argument

--- simulate.py
+++ simulate.py
@@ -1,9 +1,17 @@
 __author__ = 'monica_wang'
 from ultimatepoker import Game
+import sys
+
+try:
+    simulateTimes = int(sys.argv[1])
+except IndexError or ValueError:
+    print "Error: Input simulations times. Usage: python simulate.py [times]"
+    exit(1)
+
 
 totalGain = 0
 totalBet = 0
-simulateTimes = 1000000
+
 for i in range(simulateTimes):
     game = Game(False)
     gain, bet = game.playGame()
